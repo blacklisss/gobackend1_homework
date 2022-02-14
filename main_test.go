e1e445c020b04a03bb393b136e89d19cc557978b
blacklisss <rmjv@mail.ru>
2022-02-14 21:25:25 +0300

    Lesson 4

---
+++ main_test.go
@@ -0,0 +1,252 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+func TestUploadHandler(t *testing.T) {
+	// открываем файл, который хотим отправить
+	file, _ := os.Open("testfile")
+	defer file.Close()
+
+	// действия, необходимые для того, чтобы засунуть файл в запрос
+	// в качестве мультипарт-формы
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	// опять создаем запрос, теперь уже на /upload эндпоинт
+	req, _ := http.NewRequest(http.MethodPost, "/upload", body)
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+
+	// создаем ResponseRecorder
+	rr := httptest.NewRecorder()
+
+	// создаем заглушку файлового сервера. Для прохождения тестов
+	// нам достаточно чтобы он возвращал 200 статус
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "ok!")
+	}))
+	defer ts.Close()
+
+	uploadHandler := &UploadHandler{
+		UploadDir: "upload",
+		// таким образом мы подменим адрес файлового сервера
+		// и вместо реального, хэндлер будет стучаться на заглушку
+		// которая всегда будет возвращать 200 статус, что нам и нужна
+		HostAddr: ts.URL,
+	}
+
+	// опять же, вызываем ServeHTTP у тестируемого обработчика
+	uploadHandler.ServeHTTP(rr, req)
+
+	// Проверяем статус-код ответа
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	defer os.Remove(uploadHandler.UploadDir + "/testfile")
+	expected := `testfile`
+	if !strings.Contains(rr.Body.String(), expected) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestGetHandler(t *testing.T) {
+	// Создаем запрос с указанием нашего хендлера. Так как мы тестируем GET-эндпоинт
+	// то нам не нужно передавать тело, поэтому третьим аргументом передаем nil
+	req, err := http.NewRequest("GET", "/?name=John", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Мы создаем ResponseRecorder(реализует интерфейс http.ResponseWriter)
+	// и используем его для получения ответа
+	rr := httptest.NewRecorder()
+	handler := &Handler{}
+
+	// Наш хендлер соответствует интерфейсу http.Handler, а значит
+	// мы можем использовать ServeHTTP и напрямую указать
+	// Request и ResponseRecorder
+	handler.ServeHTTP(rr, req)
+
+	// Проверяем статус-код ответа
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Проверяем тело ответа
+	expected := `Parsed query-param with key "name": John`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestGetList(t *testing.T) {
+	// Создаем запрос с указанием нашего хендлера. Так как мы тестируем GET-эндпоинт
+	// то нам не нужно передавать тело, поэтому третьим аргументом передаем nil
+	req, err := http.NewRequest("GET", "/list", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Мы создаем ResponseRecorder(реализует интерфейс http.ResponseWriter)
+	// и используем его для получения ответа
+	rr := httptest.NewRecorder()
+	handler := &ListHandler{
+		UploadDir: "upload",
+		HostAddr:  "http://localhost:3002",
+	}
+	// Наш хендлер соответствует интерфейсу http.Handler, а значит
+	// мы можем использовать ServeHTTP и напрямую указать
+	// Request и ResponseRecorder
+	handler.ServeHTTP(rr, req)
+
+	// Проверяем статус-код ответа
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Проверяем тело ответа
+	expected := "<a href='http://localhost:3002/i.jpeg'>i.jpeg</a> 20525Bytes<br>\n<a href='http://localhost:3002/test.txt'>test.txt</a> 3Bytes<br>"
+	if strings.TrimSpace(rr.Body.String()) != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestGetListWithParamJpeg(t *testing.T) {
+	// Создаем запрос с указанием нашего хендлера. Так как мы тестируем GET-эндпоинт
+	// то нам не нужно передавать тело, поэтому третьим аргументом передаем nil
+	req, err := http.NewRequest("GET", "/list?ext=jpeg", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Мы создаем ResponseRecorder(реализует интерфейс http.ResponseWriter)
+	// и используем его для получения ответа
+	rr := httptest.NewRecorder()
+	handler := &ListHandler{
+		UploadDir: "upload",
+		HostAddr:  "http://localhost:3002",
+	}
+	// Наш хендлер соответствует интерфейсу http.Handler, а значит
+	// мы можем использовать ServeHTTP и напрямую указать
+	// Request и ResponseRecorder
+	handler.ServeHTTP(rr, req)
+
+	// Проверяем статус-код ответа
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Проверяем тело ответа
+	expected := `<a href='http://localhost:3002/i.jpeg'>i.jpeg</a> 20525Bytes<br>`
+	if strings.TrimSpace(rr.Body.String()) != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestGetListWithParamTxt(t *testing.T) {
+	// Создаем запрос с указанием нашего хендлера. Так как мы тестируем GET-эндпоинт
+	// то нам не нужно передавать тело, поэтому третьим аргументом передаем nil
+	req, err := http.NewRequest("GET", "/list?ext=txt", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Мы создаем ResponseRecorder(реализует интерфейс http.ResponseWriter)
+	// и используем его для получения ответа
+	rr := httptest.NewRecorder()
+	handler := &ListHandler{
+		UploadDir: "upload",
+		HostAddr:  "http://localhost:3002",
+	}
+	// Наш хендлер соответствует интерфейсу http.Handler, а значит
+	// мы можем использовать ServeHTTP и напрямую указать
+	// Request и ResponseRecorder
+	handler.ServeHTTP(rr, req)
+
+	// Проверяем статус-код ответа
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Проверяем тело ответа
+	expected := "<a href='http://localhost:3002/test.txt'>test.txt</a> 3Bytes<br>"
+	if strings.TrimSpace(rr.Body.String()) != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestUploadExistFile(t *testing.T) {
+	// открываем файл, который хотим отправить
+	file, _ := os.Open("test.txt")
+	defer file.Close()
+
+	// действия, необходимые для того, чтобы засунуть файл в запрос
+	// в качестве мультипарт-формы
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	// опять создаем запрос, теперь уже на /upload эндпоинт
+	req, _ := http.NewRequest(http.MethodPost, "/upload", body)
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+
+	// создаем ResponseRecorder
+	rr := httptest.NewRecorder()
+
+	// создаем заглушку файлового сервера. Для прохождения тестов
+	// нам достаточно чтобы он возвращал 200 статус
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "ok!")
+	}))
+	defer ts.Close()
+
+	uploadHandler := &UploadHandler{
+		UploadDir: "upload",
+		// таким образом мы подменим адрес файлового сервера
+		// и вместо реального, хэндлер будет стучаться на заглушку
+		// которая всегда будет возвращать 200 статус, что нам и нужна
+		HostAddr: ts.URL,
+	}
+	defer os.Remove(uploadHandler.UploadDir + "/test2.txt")
+
+	// опять же, вызываем ServeHTTP у тестируемого обработчика
+	uploadHandler.ServeHTTP(rr, req)
+
+	// Проверяем статус-код ответа
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := `test2.txt`
+	if !strings.Contains(rr.Body.String(), expected) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
